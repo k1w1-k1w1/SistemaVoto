03ce4658cfe7cca2373b9754f7f04ba0e95bea98
Patricio Quiguango <patricioquiguango.08@gmail.com>
2026-02-18 22:33:29 -0500

    Docker

--- SistemaVoto.API/Program.cs
+++ SistemaVoto.API/Program.cs
@@ -96,7 +96,7 @@ namespace SistemaVoto.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline
-            if (app.Environment.IsDevelopment())
+            //if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
